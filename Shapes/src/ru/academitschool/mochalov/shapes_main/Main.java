b3edc767522cb5a3bc310c77e034601e58dedc4e
vm230980 <mva_nsk@list.ru>
2021-02-14 12:42:52 +0700

    Исправлена задача Shapes

--- Shapes/src/ru/academitschool/mochalov/shapes_main/Main.java
+++ Shapes/src/ru/academitschool/mochalov/shapes_main/Main.java
@@ -40,13 +40,13 @@ public class Main {
         Circle circle2 = new Circle(3);
         Rectangle rectangle2 = new Rectangle(1, 3);
         Square square2 = new Square(3);
-        Triangle triangle2 = new Triangle(-1, 0, 3, 2, 1, 4);
+        Triangle triangle2 = new Triangle(-1, -1, 3, 5, 1, 4);
 
         Shape[] shapes = {circle1, rectangle1, square1, triangle1, circle2, rectangle2, square2, triangle2};
 
         System.out.println("Ширина фигуры " + circle1 + " равна " + circle1.getWidth());
 
-        System.out.println("Высота фигуры " + triangle1 + " равна " + triangle1.getHeight());
+        System.out.println("Высота фигуры " + triangle2 + " равна " + triangle2.getHeight());
 
         System.out.println("Фигура с максимальной площадью равной " +
                 getMaxAreaShape(shapes).getArea() + ", это: " + getMaxAreaShape(shapes));
